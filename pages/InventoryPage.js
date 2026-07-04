23ea055e5f3666889fbf9ce42e7ba6df59f01e7e
Andrew Doan <blooot@gmail.com>
2026-07-03 19:30:42 -0700

    Add Page Object Model structure - LoginPage and InventoryPage

---
+++ pages/InventoryPage.js
@@ -0,0 +1,33 @@
+class InventoryPage {
+    constructor(page) {
+        this.page = page;
+        this addBackpackButton = '[data-test="add-to-cart-sauce-labs-backpack"]';
+        this.cartBadge = '.shopping_cart_badge';
+        this.cartLink = '.shopping_cart_link';
+        this.sortDropdown = '[data-test="product-sort-container"]';
+        this.productNames = '.inventory_item_name';
+        this.productPrices = '.inventory_item_price';
+    }
+
+    async addItemToCart() {
+        await this.page.click(this.addBackpackButton);
+    }
+
+    async goToCart(){
+        await this.page.click(this.cartLink);
+    }
+
+    async sortBy(option){
+        await this.page.selectOption(this.sortDropdown, option);
+    }
+
+    async getProductNames(){
+        return this.page.locator(this.productNames).allTextContents();
+    }
+
+    async getProductPrices(){
+        return this.page.locator(this.productPrices).allTextContents();
+    }
+}
+
+module.exports = { InventoryPage };
